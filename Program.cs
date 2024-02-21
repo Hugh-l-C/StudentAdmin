d02c6c53b935c6e59c7a27454ebcba24a58af404
Hugh-l-C <hugh.chadwick@aru.ac.uk>
2024-02-21 10:52:08 +0000

    I made a change

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace StudentAdmin
         public static void Main(string[] args)
         {
             DatabaseManager dbm = new DatabaseManager("Students.sqlite");
-
+            //New comment
             Student john = new Student();
             john.ID = 1;
             john.Name = "John Doe";
